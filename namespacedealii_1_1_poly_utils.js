a081c0ff991e257d0a152a1f5524b4f2d921471d
fdrmrc <fdrmrc@users.noreply.github.com>
2024-01-08 10:20:10 +0000

    deploy: 89a4a4ae625e3355f2b995f88bf5f41824f0a90c

--- namespacedealii_1_1_poly_utils.js
+++ namespacedealii_1_1_poly_utils.js
@@ -1,6 +1,7 @@
 var namespacedealii_1_1_poly_utils =
 [
     [ "Rtree_visitor", "structdealii_1_1_poly_utils_1_1_rtree__visitor.html", "structdealii_1_1_poly_utils_1_1_rtree__visitor" ],
+    [ "collect_cells_for_agglomeration", "namespacedealii_1_1_poly_utils.html#a9b1f017e9d2cedf0e4362bf8a7d62419", null ],
     [ "compute_h_orthogonal", "namespacedealii_1_1_poly_utils.html#a4bbefeeca37b82d5391b9002f6a4db3f", null ],
     [ "extract_children_of_level", "namespacedealii_1_1_poly_utils.html#a36f50c815c9532815b4fc1c11a14f5b1", null ]
 ];
--- poly__utils_8h.js
+++ poly__utils_8h.js
@@ -1,6 +1,7 @@
 var poly__utils_8h =
 [
     [ "dealii::PolyUtils::Rtree_visitor< Value, Options, Translator, Box, Allocators >", "structdealii_1_1_poly_utils_1_1_rtree__visitor.html", "structdealii_1_1_poly_utils_1_1_rtree__visitor" ],
+    [ "collect_cells_for_agglomeration", "poly__utils_8h.html#a9b1f017e9d2cedf0e4362bf8a7d62419", null ],
     [ "compute_h_orthogonal", "poly__utils_8h.html#a4bbefeeca37b82d5391b9002f6a4db3f", null ],
     [ "extract_children_of_level", "poly__utils_8h.html#a36f50c815c9532815b4fc1c11a14f5b1", null ]
 ];
