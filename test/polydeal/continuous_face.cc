45e30f498501ad5a2fbfaddc8a98b23728598d6c
Marco Feder <marco.feder@sissa.it>
2024-01-20 18:13:54 +0000

    Test with neighbors of neighbors

--- test/polydeal/continuous_face.cc
+++ test/polydeal/continuous_face.cc
@@ -36,6 +36,95 @@
 #include <agglomeration_handler.h>
 #include <poly_utils.h>
 
+
+void
+test_neighbors()
+{
+  Triangulation<2> tria;
+  GridGenerator::hyper_cube(tria, -1, 1);
+  MappingQ<2> mapping(1);
+  tria.refine_global(2);
+  GridTools::Cache<2>     cached_tria(tria, mapping);
+  AgglomerationHandler<2> ah(cached_tria);
+
+  std::vector<types::global_cell_index> idxs_to_be_agglomerated = {0, 1, 2, 3};
+
+  std::vector<typename Triangulation<2>::active_cell_iterator>
+    cells_to_be_agglomerated;
+  PolyUtils::collect_cells_for_agglomeration(tria,
+                                             idxs_to_be_agglomerated,
+                                             cells_to_be_agglomerated);
+
+  std::vector<types::global_cell_index> idxs_to_be_agglomerated2 = {4, 5, 6, 7};
+
+  std::vector<typename Triangulation<2>::active_cell_iterator>
+    cells_to_be_agglomerated2;
+  PolyUtils::collect_cells_for_agglomeration(tria,
+                                             idxs_to_be_agglomerated2,
+                                             cells_to_be_agglomerated2);
+
+  std::vector<types::global_cell_index> idxs_to_be_agglomerated3 = {8,
+                                                                    9,
+                                                                    10,
+                                                                    11};
+
+  std::vector<typename Triangulation<2>::active_cell_iterator>
+    cells_to_be_agglomerated3;
+  PolyUtils::collect_cells_for_agglomeration(tria,
+                                             idxs_to_be_agglomerated3,
+                                             cells_to_be_agglomerated3);
+
+
+  std::vector<types::global_cell_index> idxs_to_be_agglomerated4 = {12,
+                                                                    13,
+                                                                    14,
+                                                                    15};
+
+  std::vector<typename Triangulation<2>::active_cell_iterator>
+    cells_to_be_agglomerated4;
+  PolyUtils::collect_cells_for_agglomeration(tria,
+                                             idxs_to_be_agglomerated4,
+                                             cells_to_be_agglomerated4);
+  // Agglomerate the cells just stored
+  ah.insert_agglomerate(cells_to_be_agglomerated);
+  ah.insert_agglomerate(cells_to_be_agglomerated2);
+  ah.insert_agglomerate(cells_to_be_agglomerated3);
+  ah.insert_agglomerate(cells_to_be_agglomerated4);
+
+  std::vector<std::vector<typename Triangulation<2>::active_cell_iterator>>
+    agglomerations{cells_to_be_agglomerated,
+                   cells_to_be_agglomerated2,
+                   cells_to_be_agglomerated3,
+                   cells_to_be_agglomerated4};
+
+  FE_DGQ<2> fe_dg(1);
+  ah.distribute_agglomerated_dofs(fe_dg);
+  ah.initialize_fe_values(QGauss<2>(1), update_JxW_values);
+
+  std::cout << "Check on neighbors and neighbors of neighbors:" << std::endl;
+  for (const auto &cell : ah.agglomeration_cell_iterators() |
+                            IteratorFilters::ActiveFEIndexEqualTo(
+                              ah.CellAgglomerationType::master))
+    {
+      unsigned int n_faces = ah.n_agglomerated_faces(cell);
+      for (unsigned int f = 0; f < n_faces; ++f)
+        {
+          const auto &neighbor = ah.agglomerated_neighbor(cell, f);
+          if (!ah.at_boundary(cell, f))
+            {
+              Assert((ah.agglomerated_neighbor(
+                          neighbor,
+                          ah.neighbor_of_agglomerated_neighbor(cell, f))
+                        ->active_cell_index() == cell->active_cell_index()),
+                     ExcMessage("Mismatch!"));
+            }
+        }
+    }
+  std::cout << "Ok" << std::endl;
+}
+
+
+
 int
 main()
 {
@@ -149,4 +238,7 @@ main()
       std::cout << std::endl;
     }
   std::cout << "Perimeter = " << perimeter << std::endl;
+  std::cout << "- - - - - - - - - - - -" << std::endl;
+
+  test_neighbors();
 }
