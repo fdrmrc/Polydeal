3e2057651684a0b9b329f59cacfafaf9b937ac69
fdrmrc <fdrmrc@users.noreply.github.com>
2024-03-06 23:48:52 +0000

    deploy: bbc60a3bdb74fd357230c9ce4dd01c89a4e7e1d9

--- namespacedealii_1_1_poly_utils.js
+++ namespacedealii_1_1_poly_utils.js
@@ -1,8 +1,13 @@
 var namespacedealii_1_1_poly_utils =
 [
+    [ "internal", "namespacedealii_1_1_poly_utils_1_1internal.html", [
+      [ "get_face_connectivity_of_cells", "namespacedealii_1_1_poly_utils_1_1internal.html#a168e98040c43aa123c04ef5de5ffd359", null ],
+      [ "get_index", "namespacedealii_1_1_poly_utils_1_1internal.html#af3f69f2eb83fd6281070f540543c1207", null ]
+    ] ],
     [ "Rtree_visitor", "structdealii_1_1_poly_utils_1_1_rtree__visitor.html", "structdealii_1_1_poly_utils_1_1_rtree__visitor" ],
     [ "collect_cells_for_agglomeration", "namespacedealii_1_1_poly_utils.html#a9b1f017e9d2cedf0e4362bf8a7d62419", null ],
     [ "compute_h_orthogonal", "namespacedealii_1_1_poly_utils.html#a4bbefeeca37b82d5391b9002f6a4db3f", null ],
     [ "extract_children_of_level", "namespacedealii_1_1_poly_utils.html#a36f50c815c9532815b4fc1c11a14f5b1", null ],
-    [ "interpolate_to_fine_grid", "namespacedealii_1_1_poly_utils.html#ad10be894e09c9fd78712c6e4ad8fad14", null ]
+    [ "interpolate_to_fine_grid", "namespacedealii_1_1_poly_utils.html#ad10be894e09c9fd78712c6e4ad8fad14", null ],
+    [ "partition_locally_owned_regions", "namespacedealii_1_1_poly_utils.html#a03c416000585c023db89c43a3bca9b6c", null ]
 ];
--- poly__utils_8h.js
+++ poly__utils_8h.js
@@ -4,5 +4,8 @@ var poly__utils_8h =
     [ "collect_cells_for_agglomeration", "poly__utils_8h.html#a9b1f017e9d2cedf0e4362bf8a7d62419", null ],
     [ "compute_h_orthogonal", "poly__utils_8h.html#a4bbefeeca37b82d5391b9002f6a4db3f", null ],
     [ "extract_children_of_level", "poly__utils_8h.html#a36f50c815c9532815b4fc1c11a14f5b1", null ],
-    [ "interpolate_to_fine_grid", "poly__utils_8h.html#ad10be894e09c9fd78712c6e4ad8fad14", null ]
+    [ "get_face_connectivity_of_cells", "poly__utils_8h.html#a168e98040c43aa123c04ef5de5ffd359", null ],
+    [ "get_index", "poly__utils_8h.html#af3f69f2eb83fd6281070f540543c1207", null ],
+    [ "interpolate_to_fine_grid", "poly__utils_8h.html#ad10be894e09c9fd78712c6e4ad8fad14", null ],
+    [ "partition_locally_owned_regions", "poly__utils_8h.html#a03c416000585c023db89c43a3bca9b6c", null ]
 ];
