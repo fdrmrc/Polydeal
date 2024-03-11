b6cba53ba362dc1b18f511e882d14d015ce2883e
fdrmrc <fdrmrc@users.noreply.github.com>
2024-03-11 23:30:04 +0000

    deploy: 2b7a91d393e28df6a7280f85626fd93217044a5e

--- namespacedealii_1_1_poly_utils.js
+++ namespacedealii_1_1_poly_utils.js
@@ -6,7 +6,7 @@ var namespacedealii_1_1_poly_utils =
     ] ],
     [ "Rtree_visitor", "structdealii_1_1_poly_utils_1_1_rtree__visitor.html", "structdealii_1_1_poly_utils_1_1_rtree__visitor" ],
     [ "collect_cells_for_agglomeration", "namespacedealii_1_1_poly_utils.html#a9b1f017e9d2cedf0e4362bf8a7d62419", null ],
-    [ "compute_h_orthogonal", "namespacedealii_1_1_poly_utils.html#a4bbefeeca37b82d5391b9002f6a4db3f", null ],
+    [ "compute_h_orthogonal", "namespacedealii_1_1_poly_utils.html#a692c4e431675a877af2ebcdfa416129b", null ],
     [ "extract_children_of_level", "namespacedealii_1_1_poly_utils.html#a36f50c815c9532815b4fc1c11a14f5b1", null ],
     [ "interpolate_to_fine_grid", "namespacedealii_1_1_poly_utils.html#ad10be894e09c9fd78712c6e4ad8fad14", null ],
     [ "partition_locally_owned_regions", "namespacedealii_1_1_poly_utils.html#a03c416000585c023db89c43a3bca9b6c", null ]
