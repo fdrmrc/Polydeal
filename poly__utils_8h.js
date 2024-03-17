c88d780e55a2aa86bd59cdd7619d6432f91d997e
fdrmrc <fdrmrc@users.noreply.github.com>
2024-03-17 23:16:15 +0000

    deploy: fa09df0a535ae0d172147d19395875824b1a57f5

--- poly__utils_8h.js
+++ poly__utils_8h.js
@@ -3,6 +3,7 @@ var poly__utils_8h =
     [ "dealii::PolyUtils::Rtree_visitor< Value, Options, Translator, Box, Allocators >", "structdealii_1_1_poly_utils_1_1_rtree__visitor.html", "structdealii_1_1_poly_utils_1_1_rtree__visitor" ],
     [ "collect_cells_for_agglomeration", "poly__utils_8h.html#a9b1f017e9d2cedf0e4362bf8a7d62419", null ],
     [ "compute_h_orthogonal", "poly__utils_8h.html#a692c4e431675a877af2ebcdfa416129b", null ],
+    [ "compute_quality_metrics", "poly__utils_8h.html#a8d1ece63def3bff3f7c8baf781066bdd", null ],
     [ "extract_children_of_level", "poly__utils_8h.html#a36f50c815c9532815b4fc1c11a14f5b1", null ],
     [ "get_face_connectivity_of_cells", "poly__utils_8h.html#a168e98040c43aa123c04ef5de5ffd359", null ],
     [ "get_index", "poly__utils_8h.html#af3f69f2eb83fd6281070f540543c1207", null ],
