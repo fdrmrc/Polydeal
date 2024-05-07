197253626d7c7658fd48098747e55438aa388be6
Marco Feder <marco.feder@sissa.it>
2024-05-07 18:29:20 +0000

    Provide IndexSet to DynamicSparsityPattern

--- include/poly_utils.h
+++ include/poly_utils.h
@@ -559,7 +559,9 @@ namespace dealii::PolyUtils
     const IndexSet &locally_owned_dofs_agglo = agglo_dh.locally_owned_dofs();
 
 
-    DynamicSparsityPattern dsp(output_dh->n_dofs(), agglo_dh.n_dofs());
+    DynamicSparsityPattern dsp(output_dh->n_dofs(),
+                               agglo_dh.n_dofs(),
+                               output_dh->locally_owned_dofs());
 
     std::vector<types::global_dof_index> agglo_dof_indices(fe.dofs_per_cell);
     std::vector<types::global_dof_index> standard_dof_indices(fe.dofs_per_cell);
