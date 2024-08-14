443e4db574c8f738389ec7a9f035596d427f9bcd
Marco Feder <marco.feder@sissa.it>
2024-08-14 09:19:24 +0000

    Move general utility to compute interpolation matrix to utilities

--- include/agglomeration_handler.h
+++ include/agglomeration_handler.h
@@ -1189,16 +1189,8 @@ void
 AgglomerationHandler<dim, spacedim>::connect_hierarchy(
   const CellsAgglomerator<dim, RtreeType> &agglomerator)
 {
-<<<<<<< HEAD
-<<<<<<< HEAD
   // Perform deep copy
   parent_child_info        = agglomerator.parent_node_to_children_nodes;
-=======
-  parent_child_info = &agglomerator.parent_node_to_children_nodes;
->>>>>>> WIP: working. To test: coarse grid solver too expensive
-=======
-  parent_child_info        = agglomerator.parent_node_to_children_nodes;
->>>>>>> Generalize to more levels
   present_extraction_level = agglomerator.extraction_level;
 }
 
